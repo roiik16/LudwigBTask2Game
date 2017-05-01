bf45e8df189fc00a6e631609937179cf190b7c72
roiik16 <roik1234@gmail.com>
2017-05-01 21:14:37 +0200

    Powerups done
    NEXT > Spawn the powerups random and lock blocks from jumping with
    player and falling sideways.
    
    WTF LIGHTING??

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -1,17 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Playermovement : MonoBehaviour {
 
+    //Physics component
     public Rigidbody rb;
 
+    //setting player movement speed
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    //Jump speed
+    public float jumpForce = 500f;
+
+    Transform groundCheck;
+
+    //Check when the box can jump
+    public bool isGrounded = true;
+
+    private bool isCoroutineExecuting = false;
+
+    bool jumpunlocked = false;
+
+    bool slowpower = false;
+
+    void Awake()
     {
+        groundCheck = transform.Find("groundCheck");
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
+
+        //PLAYER CONTROLS
         if (Input.GetKey("d"))
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
@@ -26,5 +52,43 @@ public class Playermovement : MonoBehaviour {
         {
             FindObjectOfType<GameManager>().EndGame();
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded && jumpunlocked)
+        {
+           rb.AddForce(Vector3.up * jumpForce);
+        }
+
+
+        //SLOWPOWERUP INITIATE
+        if (slowpower == true)
+        {
+            StartCoroutine(Slowpowerup());
+        }
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        //UNLOCK JUMP POWERUP
+        if (c.tag == "jumpPower")
+        {
+            jumpunlocked = true;
+        }
+
+
+        //UNLOCK SLOW POWERUP
+        if (c.tag == "slowPower")
+        {
+            slowpower = true;
+        }
+        
+    }
+
+
+     IEnumerator Slowpowerup()
+    {        
+        forwardForce = 200f;
+        yield return new WaitForSeconds(2);
+        forwardForce = 2000f;
     }
 }
